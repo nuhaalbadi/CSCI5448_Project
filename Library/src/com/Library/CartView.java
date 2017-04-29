2724b19785ba6c8369d3e8b8dc849ffb31a76b3c
khandady <kdudetech@gmail.com>
2017-04-29 16:16:37 -0600

    Added Shopping Cart View

---
+++ Library/src/com/Library/CartView.java
@@ -0,0 +1,90 @@
+package com.Library;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+
+public class CartView {
+    JButton r;
+    CartView() {
+
+        JLabel title=new JLabel("Shopping Cart:");
+        JLabel q = new JLabel("Enter ISBN:");
+        JButton b = new JButton("Add to Cart");
+        JButton r = new JButton("Return");
+        JTextField ISBN=new JTextField(20);
+        JTable table=new JTable();
+        JScrollPane pane=new JScrollPane(table);
+        double total = 0.00;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root", "admin");
+            Statement st= myConnection.createStatement();
+            ResultSet res=st.executeQuery("select * from cart");
+            ResultSetMetaData mdt = res.getMetaData();
+            int columns = mdt.getColumnCount();
+            Vector columnNames = new Vector();
+            Vector data = new Vector();
+            for (int col = 2; col <= columns; col++){
+                columnNames.addElement(mdt.getColumnName(col) );
+            }
+            while(res.next()){
+                Vector rows = new Vector(columns);
+                for (int col = 2; col <= columns; col++){
+                    rows.addElement( res.getObject(col) );
+                    if(col == 3){
+                        total += Double.parseDouble(" " + res.getObject(col));
+                    }
+                }
+                data.addElement(rows);
+            }
+
+            DefaultTableModel list=new DefaultTableModel(data, columnNames);
+            table.setModel(list);
+        }
+        catch(Exception ex){
+            System.out.println("Checkout Failed");
+            JOptionPane.showMessageDialog(b,"invalid username or password");
+        }
+
+        JFrame j = new JFrame();
+        j.setLayout(null);
+        title.setBounds(10,10,100,20);
+        q.setBounds(100,250,180,20);
+        b.setBounds(300,250,180,20);
+        r.setBounds(250,300,80,20);
+
+        ISBN.setBounds(200,250,80,20);
+        pane.setBounds(10,30,500,170);
+        pane.setVisible(false);
+        j.add(title);
+        j.add(b);
+        j.add(pane);
+        j.add(q);
+        j.add(r);
+        j.add(ISBN);
+        pane.setVisible(true);
+        j.setSize(600,500);
+        j.setVisible(true);
+
+        r.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new CustomerView();
+                j.setVisible(false);
+            }
+        });
+
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new CartView();
+                j.setVisible(false);
+            }
+        });
+    }
+
+}
