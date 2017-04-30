b34a19f3a2dadddf6e5daf8a59df3d2efd339900
nuhaalbadi <noha.albadi@hotmail.com>
2017-04-29 21:39:03 -0600

    Added Search customer for Admin

--- Library/src/com/Library/AdminPortal.java
+++ Library/src/com/Library/AdminPortal.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 import java.sql.*;
 
 public class AdminPortal extends JFrame implements ActionListener {
-    JButton btn1, btn2;
+    JButton btn1, btn2, btn3, btn4;
     JLabel l1;
 
     AdminPortal()
@@ -24,14 +24,22 @@ public class AdminPortal extends JFrame implements ActionListener {
         btn1.addActionListener(this);
         btn2 = new JButton("Add New Media");
         btn2.addActionListener(this);
+        btn3 = new JButton("Search Customer");
+        btn3.addActionListener(this);
+        btn4 = new JButton("Logout");
+        btn4.addActionListener(this);
 
         l1.setBounds(80, 30, 400, 30);
         btn1.setBounds(300, 70, 150, 30);
         btn2.setBounds(300, 110, 150, 30);
+        btn3.setBounds(300, 150, 150, 30);
+        btn4.setBounds(300,190,150,30);
 
         add(l1);
         add(btn1);
         add(btn2);
+        add(btn3);
+        add(btn4);
         setVisible(true);
     }
 
@@ -42,9 +50,19 @@ public class AdminPortal extends JFrame implements ActionListener {
             new AddLibrarian();
             setVisible(false);
         }
-        else if (e.getSource() == btn2)
+        else if (e.getSource() == btn2) // add media 
         {
             new AddMedia();
+            setVisible(false);
+        }
+        else if (e.getSource() == btn3) // Search Customer
+        {
+           new SearchCustomer();
+            setVisible(false);
+        }
+        else if (e.getSource() == btn4) // logout
+        {
+        	new LoginForm();
             setVisible(false);
         }
     }
