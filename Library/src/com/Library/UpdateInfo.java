8d9ba3e35c07147148c784f13eb2fef77c9bf96e
mukundmatre <muat2215@colorado.edu>
2017-05-01 10:28:31 -0600

    Adding Update Customer Info Tab to Librarian Portal

--- Library/src/com/Library/UpdateInfo.java
+++ Library/src/com/Library/UpdateInfo.java
@@ -1,4 +1,10 @@
+
 package com.Library;
+
+/**
+ *
+ * @author mmsatre
+ */
 
 import java.awt.*;
 import java.sql.*;
@@ -7,12 +13,20 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-public class SearchCustomer {
-    JButton r;
-    SearchCustomer() {
-
-        JLabel title=new JLabel("Enter Name:");
+public class UpdateInfo {
+    UpdateInfo() {
+    JLabel title=new JLabel("Enter Name:");
+    JTextField tf1, tf2, tf5, tf6;
+    JLabel l2,l3,l4,l5;    
+        l2 = new JLabel("Name:");
+        l3 = new JLabel("Email:");
+        l4 = new JLabel("Phone No:");
+        l5 = new JLabel("Address:");
         JTextField t=new JTextField(20);
+        tf1 = new JTextField();
+        tf2 = new JTextField();
+        tf5 = new JTextField();
+        tf6 = new JTextField();
         JButton b = new JButton("Search");
         JButton r = new JButton("Return");
         JTable table=new JTable();
@@ -24,7 +38,15 @@ public class SearchCustomer {
         title.setBounds(10,10,100,20);
         t.setBounds(120,10,100,20);
         b.setBounds(250,10,80,20);
-        r.setBounds(250,400,80,20);
+        r.setBounds(250,700,80,20);
+        l2.setBounds(80, 450, 200, 30);
+        l3.setBounds(80, 500, 200, 30);
+        l4.setBounds(80, 550, 200, 30);
+        l5.setBounds(80, 600, 200, 30);
+        tf1.setBounds(300, 450, 200, 30);
+        tf2.setBounds(300, 500, 200, 30);
+        tf5.setBounds(300, 550, 200, 30);
+        tf6.setBounds(300, 600, 200, 30);
         pane.setBounds(10,70,500,170);
         pane.setVisible(false);
         j.add(title);
@@ -32,13 +54,21 @@ public class SearchCustomer {
         j.add(b);
         j.add(pane);
         j.add(r);
+        j.add(l2);
+        j.add(tf1);
+        j.add(l3);
+        j.add(tf2);
+        j.add(l4);
+        j.add(tf5);
+        j.add(l5);
+        j.add(tf6);
         pane.setVisible(false);
-        j.setSize(600,500);
+        j.setSize(1200,1000);
         j.setVisible(true);
 
         r.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                new AdminPortal();
+                new LibrarianPortal();
                 j.setVisible(false);
             }
         });
@@ -75,6 +105,6 @@ public class SearchCustomer {
             }
         });
     }
-
-
+    
+ 
 }
