23661a7027c24853055f9f5f4b2471fc240636c6
khandady <kdudetech@gmail.com>
2017-05-01 21:45:29 -0600

    Added check for update media

--- Library/src/com/Library/AdminPortal.java
+++ Library/src/com/Library/AdminPortal.java
@@ -1,13 +1,15 @@
 package com.Library;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
+import java.util.Vector;
 
 public class AdminPortal extends JFrame implements ActionListener {
-    JButton btn1, btn2, btn3, btn4, btn5;
-    JLabel l1;
+    JButton btn1, btn2, btn3, btn4;
+    JLabel l1,l2;
 
     AdminPortal()
     {
@@ -20,6 +22,46 @@ public class AdminPortal extends JFrame implements ActionListener {
         l1.setForeground(Color.blue);
         l1.setFont(new Font("Serif", Font.BOLD, 20));
 
+        l2 = new JLabel("New Media Requests!");
+        l2.setForeground(Color.red);
+        l2.setFont(new Font("Serif", Font.BOLD, 20));
+        l2.setVisible(false);
+        int numRows = 0;
+        int currRows = 0;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root", "admin");
+            Statement st= myConnection.createStatement();
+            ResultSet res=st.executeQuery("SELECT COUNT(*) FROM mediarequests");
+
+            res.next();
+            numRows = res.getInt(1);
+            res.close();
+
+            st= myConnection.createStatement();
+            res=st.executeQuery("SELECT * FROM oldrequestnum");
+
+            res.next();
+            currRows = res.getInt(1);
+            res.close();
+        }
+        catch(Exception ex){
+            System.out.println("Count Failed");
+        }
+
+        if(numRows > currRows){
+            l2.setVisible(true);
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root", "admin");
+                PreparedStatement pst = myConnection.prepareStatement("update oldrequestnum set Number='"+numRows+"' where Number='"+currRows+"'");
+                pst.execute();
+            }
+            catch(Exception ex){
+                System.out.println("Update Failed");
+            }
+        }
+
         btn1 = new JButton("Add New Librarian");
         btn1.addActionListener(this);
         btn2 = new JButton("Add New Media");
@@ -28,22 +70,20 @@ public class AdminPortal extends JFrame implements ActionListener {
         btn3.addActionListener(this);
         btn4 = new JButton("Logout");
         btn4.addActionListener(this);
-        btn5 = new JButton("View Requests");
-        btn5.addActionListener(this);
 
-        l1.setBounds(150, 30, 400, 30);
-        btn1.setBounds(175, 70, 200, 30);
-        btn2.setBounds(175, 110, 200, 30);
-        btn3.setBounds(175, 150, 200, 30);
-        btn4.setBounds(175,230,200,30);
-        btn5.setBounds(175, 190, 200, 30);
+        l1.setBounds(80, 30, 400, 30);
+        l2.setBounds(300,250,400,30);
+        btn1.setBounds(300, 70, 150, 30);
+        btn2.setBounds(300, 110, 150, 30);
+        btn3.setBounds(300, 150, 150, 30);
+        btn4.setBounds(300,190,150,30);
 
         add(l1);
         add(btn1);
         add(btn2);
         add(btn3);
         add(btn4);
-        add(btn5);
+        add(l2);
         setVisible(true);
     }
 
@@ -54,24 +94,19 @@ public class AdminPortal extends JFrame implements ActionListener {
             new AddLibrarian();
             setVisible(false);
         }
-        else if (e.getSource() == btn2) // add media 
+        else if (e.getSource() == btn2) // add media
         {
             new AddMedia();
             setVisible(false);
         }
         else if (e.getSource() == btn3) // Search Customer
         {
-           new SearchCustomer();
+            new SearchCustomer();
             setVisible(false);
         }
         else if (e.getSource() == btn4) // logout
         {
-        	new LoginForm();
-            setVisible(false);
-        }
-        else if (e.getSource() == btn5)
-        {
-            new RequestsView();
+            new LoginForm();
             setVisible(false);
         }
     }
